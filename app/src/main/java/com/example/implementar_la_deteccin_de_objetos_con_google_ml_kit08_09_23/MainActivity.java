c3bb8bf3c0ca9ee3c073706daf1b4729b0c19060
kingoloza <124217307+kingoloza@users.noreply.github.com>
2023-09-08 00:41:19 -0500

    Initial commit

---
+++ app/src/main/java/com/example/implementar_la_deteccin_de_objetos_con_google_ml_kit08_09_23/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.implementar_la_deteccin_de_objetos_con_google_ml_kit08_09_23;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
